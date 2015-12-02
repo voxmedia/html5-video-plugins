d860376cc4cb182ecf69d6e82b60b0838ca0623a
Aldi Gunawan <agunawan@ooyala.com>
2015-12-02 14:11:46 -0800

    [shaka] prototype of Shaka dash player

---
+++ js/shaka/shaka.js
@@ -0,0 +1,454 @@
+/*
+ * Video plugin template
+ * This template can serve as an example of a Video Technology Plugin
+ * version: 0.1
+ */
+
+(function(_, $) {
+  /**
+   * @class TemplateVideoFactory
+   * @classdesc Factory for creating video player objects that use HTML5 video tags.
+   * @property {string} name The name of the plugin
+   * @property {boolean} ready The readiness of the plugin for use.  True if elements can be created.
+   * @property {object} streams An array of supported encoding types (ex. m3u8, mp4)
+   */
+  var TemplateVideoFactory = function() {
+    this.name = "shaka_dash";
+    this.encodings = ["dash"];
+    var currentInstances = 0;
+
+    // This module defaults to ready because no setup or external loading is required
+    this.ready = false;
+
+    /**
+     * Creates a video player instance using TemplateVideoWrapper.
+     * @public
+     * @method TemplateVideoFactory#create
+     * @param {object} parentContainer The jquery div that should act as the parent for the video element
+     * @param {string} domId The dom id of the video player instance to create
+     * @param {object} ooyalaVideoController A reference to the video controller in the Ooyala player
+     * @param {object} css The css to apply to the video element
+     * @returns {object} A reference to the wrapper for the newly created element
+     */
+    this.create = function(parentContainer, domId, ooyalaVideoController, css) {
+      if (this.maxSupportedElements > 0 && currentInstances >= this.maxSupportedElements) {
+        return;
+      }
+
+      var video = $("<video>");
+      video.attr("class", "video");
+      video.attr("id", domId);
+
+      // require the site to setup CORS correctly to enable track url and src url to come from different domains
+      // Temporarily remove this.  When we implement closed captions we need to optionally add this back in.
+      // It should not be used for ad videos.  It should be used for the main video.
+      //video.attr("crossorigin", "anonymous");
+      //video.css(css);
+
+      // enable airplay for iOS
+      // http://developer.apple.com/library/safari/#documentation/AudioVideo/Conceptual/AirPlayGuide/OptingInorOutofAirPlay/OptingInorOutofAirPlay.html
+      if (Platform.isIos) {
+        video.attr("x-webkit-airplay", "allow");
+      }
+
+      var wrapper = new TemplateVideoWrapper(domId, video[0]);
+      currentInstances++;
+      wrapper.controller = ooyalaVideoController;
+
+      // TODO: Wait for loadstart before calling this?
+      wrapper.subscribeAllEvents();
+
+      parentContainer.append(video);
+
+      // On Android, we need to "activate" the video on a click so we can control it with JS later on mobile
+      if (Platform.isAndroid) {
+        wrapper.play();
+        wrapper.pause();
+      }
+      return wrapper;
+    };
+
+    /**
+     * Destroys the video technology factory.
+     * @public
+     * @method TemplateVideoFactory#destroy
+     */
+    this.destroy = function() {
+      this.ready = false;
+      this.encodings = [];
+      this.create = function() {};
+    };
+
+    /**
+     * Represents the max number of support instances of video elements that can be supported on the
+     * current platform. -1 implies no limit.
+     * @public
+     * @property TemplateVideoFactory#maxSupportedElements
+     */
+    this.maxSupportedElements = 1;
+
+    /**
+     * Returns the number of video elements currently instantiated.
+     * @public
+     * @method TemplateVideoFactory#getCurrentNumberOfInstances
+     * @returns {int} The number of video elements created by this factory that have not been destroyed
+     */
+    this.getCurrentNumberOfInstances = function() {
+    };
+  };
+
+  /**
+   * @class TemplateVideoWrapper
+   * @classdesc Player object that wraps the video element.
+   * @param {string} domId The dom id of the video player element
+   * @param {object} video The core video object to wrap
+   * @property {object} controller A reference to the Ooyala Video Tech Controller
+   * @property {boolean} disableNativeSeek When true, the plugin should supress or undo seeks that come from
+   *                                       native video controls
+   */
+  var TemplateVideoWrapper = function(domId, video) {
+    var _video = video;
+    var listeners = {};
+    shaka.polyfill.installAll();
+    var _player = new shaka.player.Player(_video);
+
+    this.controller = {};
+    this.disableNativeSeek = false;
+
+    /************************************************************************************/
+    // Required. Methods that Video Controller, Destroy, or Factory call
+    /************************************************************************************/
+
+    /**
+     * Subscribes to all events raised by the video element.
+     * This is called by the Factory during creation.
+     * @public
+     * @method TemplateVideoWrapper#subscribeAllEvents
+     */
+    this.subscribeAllEvents = function() {
+      listeners = { "play": _.bind(raisePlayEvent, this),
+                    "playing": _.bind(raisePlayingEvent, this),
+                    "ended": _.bind(raiseEndedEvent, this),
+                    "error": _.bind(raiseErrorEvent, this),
+                    "seeking": _.bind(raiseSeekingEvent, this),
+                    "seeked": _.bind(raiseSeekedEvent, this),
+                    "pause": _.bind(raisePauseEvent, this),
+                    "ratechange": _.bind(raiseRatechangeEvent, this),
+                    "stalled": _.bind(raiseStalledEvent, this),
+                    "volumechange": _.bind(raiseVolumeEvent, this),
+                    "volumechangeNew": _.bind(raiseVolumeEvent, this),
+                    "waiting": _.bind(raiseWaitingEvent, this),
+                    "timeupdate": _.bind(raiseTimeUpdate, this),
+                    "durationchange": _.bind(raiseDurationChange, this),
+                    "progress": _.bind(raiseProgress, this),
+                    "canplaythrough": _.bind(raiseCanPlayThrough, this),
+                    "webkitbeginfullscreen": _.bind(raiseFullScreenBegin, this),
+                    "webkitendfullscreen": _.bind(raiseFullScreenEnd, this)
+                  };
+      _.each(listeners, function(v, i) { $(_video).on(i, v); }, this);
+    };
+
+    /**
+     * Unsubscribes all events from the video element.
+     * This should be called by the destroy function.
+     * @public
+     * @method TemplateVideoWrapper#unsubscribeAllEvents
+     */
+    this.unsubscribeAllEvents = function() {
+      _.each(listeners, function(v, i) { $(_video).off(i, v); }, this);
+    };
+
+    /**
+     * Sets the url of the video.
+     * @public
+     * @method TemplateVideoWrapper#setVideoUrl
+     * @param {string} url The new url to insert into the video element's src attribute
+     * @returns {boolean} True or false indicating success
+     */
+    this.setVideoUrl = function(url) {
+      var source = new shaka.player.DashVideoSource(url, null);
+      var promise = _player.load(source);
+      return true;
+    };
+
+    /**
+     * Loads the current stream url in the video element; the element should be left paused.
+     * @public
+     * @method TemplateVideoWrapper#load
+     * @param {boolean} rewind True if the stream should be set to time 0
+     */
+    this.load = function(rewind) {
+    };
+
+    /**
+     * Sets the initial time of the video playback.
+     * @public
+     * @method TemplateVideoWrapper#setInitialTime
+     * @param {number} initialTime The initial time of the video (seconds)
+     */
+    this.setInitialTime = function(initialTime) {
+    };
+
+    /**
+     * Triggers playback on the video element.
+     * @public
+     * @method TemplateVideoWrapper#play
+     */
+    this.play = function() {
+      promise.then(function() { _video.play(); });
+    };
+
+    /**
+     * Triggers a pause on the video element.
+     * @public
+     * @method TemplateVideoWrapper#pause
+     */
+    this.pause = function() {
+      promise.then(function() { _video.pause(); });
+    };
+
+    /**
+     * Triggers a seek on the video element.
+     * @public
+     * @method TemplateVideoWrapper#seek
+     * @param {number} time The time to seek the video to (in seconds)
+     */
+    this.seek = function(time) {
+    };
+
+    /**
+     * Triggers a volume change on the video element.
+     * @public
+     * @method TemplateVideoWrapper#setVolume
+     * @param {number} volume A number between 0 and 1 indicating the desired volume percentage
+     */
+    this.setVolume = function(volume) {
+    };
+
+    /**
+     * Gets the current time position of the video.
+     * @public
+     * @method TemplateVideoWrapper#getCurrentTime
+     * @returns {number} The current time position of the video (seconds)
+     */
+    this.getCurrentTime = function() {
+    };
+
+    /**
+     * Applies the given css to the video element.
+     * @public
+     * @method TemplateVideoWrapper#applyCss
+     * @param {object} css The css to apply in key value pairs
+     */
+    this.applyCss = function(css) {
+    };
+
+    /**
+     * Destroys the individual video element.
+     * @public
+     * @method TemplateVideoWrapper#destroy
+     */
+    this.destroy = function() {
+      // Pause the video
+      // Reset the source
+      // Unsubscribe all events
+      this.unsubscribeAllEvents();
+      // Remove the element
+    };
+
+
+    // **********************************************************************************/
+    // Example callback methods
+    // **********************************************************************************/
+
+    var raisePlayEvent = function(event) {
+      this.controller.notify(this.controller.EVENTS.PLAY, { url: event.target.src });
+    };
+
+    var raisePlayingEvent = function() {
+      console.log("***************** PLAYING");
+      this.controller.notify(this.controller.EVENTS.PLAYING);
+    };
+
+    var raiseEndedEvent = function() {
+      this.controller.notify(this.controller.EVENTS.ENDED);
+    };
+
+    var raiseErrorEvent = function(event) {
+      console.log("***************** ERROR: ");
+      console.log(event.target.error);
+      //var code = event.target.error ? event.target.error.code : -1;
+      //this.controller.notify(this.controller.EVENTS.ERROR, { "errorcode" : code });
+    };
+
+    var raiseSeekingEvent = function() {
+      this.controller.notify(this.controller.EVENTS.SEEKING);
+    };
+
+    var raiseSeekedEvent = function() {
+      this.controller.notify(this.controller.EVENTS.SEEKED);
+    };
+
+    var raisePauseEvent = function() {
+      this.controller.notify(this.controller.EVENTS.PAUSED);
+    };
+
+    var raiseRatechangeEvent = function() {
+      this.controller.notify(this.controller.EVENTS.RATE_CHANGE);
+    };
+
+    var raiseStalledEvent = function() {
+      this.controller.notify(this.controller.EVENTS.STALLED);
+    };
+
+    var raiseVolumeEvent = function(event) {
+      this.controller.notify(this.controller.EVENTS.VOLUME_CHANGE, { "volume" : event.target.volume });
+    };
+
+    var raiseWaitingEvent = function() {
+      this.controller.notify(this.controller.EVENTS.WAITING);
+    };
+
+    var raiseTimeUpdate = function(event) {
+      raisePlayhead(this.controller.EVENTS.TIME_UPDATE, event);
+    };
+
+    var raiseDurationChange = function(event) {
+      raisePlayhead(this.controller.EVENTS.DURATION_CHANGE, event);
+    };
+
+    var raisePlayhead = _.bind(function(eventname, event) {
+      this.controller.notify(eventname,
+                             { "currentTime" : event.target.currentTime,
+                               "duration" : event.target.duration,
+                               "buffer" : 10,
+                               "seekRange" : { "begin" : 0, "end" : 10 } });
+    }, this);
+
+    var raiseProgress = function(event) {
+      this.controller.notify(this.controller.EVENTS.PROGRESS,
+                             { "currentTime": event.target.currentTime,
+                               "duration": event.target.duration,
+                               "buffer": 10,
+                               "seekRange": { "begin": 0, "end": 10 } });
+    };
+
+    var raiseCanPlayThrough = function() {
+      this.controller.notify(this.controller.EVENTS.BUFFERED);
+    };
+
+    var raiseFullScreenBegin = function(event) {
+      this.controller.notify(this.controller.EVENTS.FULLSCREEN_CHANGED,
+                             { "isFullScreen" : true, "paused" : event.target.paused });
+    };
+
+    var raiseFullScreenEnd = function(event) {
+      this.controller.notify(this.controller.EVENTS.FULLSCREEN_CHANGED,
+                             { "isFullScreen" : false, "paused" : event.target.paused });
+    };
+  };
+
+  /**
+   * @class Platform
+   * @classdesc Functions that provide platform information
+   */
+  var Platform = {
+    /**
+     * Checks if the system is running on iOS.
+     * @private
+     * @method Platform#isIos
+     * @returns {boolean} True if the system is running on iOS
+     */
+    isIos: (function() {
+      var platform = window.navigator.platform;
+      return !!(platform.match(/iPhone/) || platform.match(/iPad/) || platform.match(/iPod/));
+    })(),
+
+    /**
+     * Checks if the system is an iPad
+     * @private
+     * @method Platform#isIpad
+     * @returns {boolean} True if the system is an Ipad
+     */
+    isIpad: (function() {
+      return !!window.navigator.platform.match(/iPad/);
+    })(),
+
+    /**
+     * Checks if the player is running in Chrome.
+     * @private
+     * @method Platform#isChrome
+     * @returns {boolean} True if the player is running in chrome
+     */
+    isChrome: (function() {
+      return !!window.navigator.userAgent.match(/Chrome/);
+    })(),
+
+    /**
+     * Checks if the player is running in Safari.
+     * @private
+     * @method Platform#isSafari
+     * @returns {boolean} True if the player is running in Safari
+     */
+    isSafari: (function() {
+      return (!!window.navigator.userAgent.match(/AppleWebKit/) &&
+              !window.navigator.userAgent.match(/Chrome/));
+    })(),
+
+    /**
+     * Gets the iOS major version.
+     * @private
+     * @method Platform#iosMajorVersion
+     * @returns {?number} The iOS major version; null if the system is not running iOS
+     */
+    iosMajorVersion: (function(){
+      try {
+        if (window.navigator.userAgent.match(/(iPad|iPhone|iPod)/)) {
+          return parseInt(window.navigator.userAgent.match(/OS (\d+)/)[1], 10);
+        } else {
+          return null;
+        }
+      } catch (err) {
+        return null;
+      }
+    })(),
+
+    /**
+     * Checks if the player is running on an Android device.
+     * @private
+     * @method Platform#isAndroid
+     * @returns {boolean} True if the player is running on an Android device
+     */
+    isAndroid: (function(){
+      return !!window.navigator.appVersion.match(/Android/);
+    })(),
+
+    /**
+     * Checks if the player is running on an Android device of version 4 or later.
+     * @private
+     * @method Platform#isAndroid4Plus
+     * @returns {boolean} True if the player is running on an Android device of version 4 or later
+     */
+    isAndroid4Plus: (function(){
+      if (!window.navigator.appVersion.match(/Android/)) return false;
+      var device = window.navigator.appVersion.match(/Android [1-9]/) || [];
+      return (_.first(device) || "").slice(-1) >= "4";
+    })(),
+
+    /**
+     * Checks if the player is running in Safari.
+     * @private
+     * @method Platform#isSafari
+     * @returns {boolean} True if the player is running in safari
+     */
+    chromeMajorVersion: (function(){
+      try {
+        return parseInt(window.navigator.userAgent.match(/Chrome.([0-9]*)/)[1], 10);
+      } catch(err) {
+        return null;
+      }
+    })(),
+  };
+
+  OO.Video.plugin(new TemplateVideoFactory());
+}(OO._, OO.$));
